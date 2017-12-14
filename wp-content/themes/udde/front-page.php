33c6007eea157ffe0d0453cf6e6e3384f3352e6a
Emma Uddesson <emma.uddesson@gmail.com>
2017-12-14 19:29:00 +0100

    Solution for "responsive" thumbnails

--- wp-content/themes/udde/front-page.php
+++ wp-content/themes/udde/front-page.php
@@ -25,28 +25,37 @@ Template name: Front Page
             $display_four_posts = new WP_Query( 'no_found_rows=true&posts_per_page=4' ); 
 
             if ( $display_four_posts->have_posts() ) : ?>
+                <div class="row">
 
-                    <div class="row">
-
-                        <!-- Loop -->
-                        <?php  while ( $display_four_posts->have_posts() ) : $display_four_posts->the_post(); ?>
-                    
-                            <!-- Content in the loop -->
-                            <div class="col-10 col-md-8 col-lg-4 offset-1 offset-md-2 offset-lg-1 post-content">
-                                <a><?php the_category(); ?></a>
-                                <div class="thumbnail">
-                                    <?php 
-                                    // the_post_thumbnail(); 
-                                    ?>
-                                </div>
-                                <?php echo '<h1><a href="'.get_permalink( $id ).'">'.get_the_title( $id ).'</a></h1>'; ?>
-                                <p><?php the_excerpt(); ?></p>
-                                <a id=#readmore>Läs mer</a>                           
-                            </div>
+                    <!-- Loop -->
+                    <?php  while ( $display_four_posts->have_posts() ) : $display_four_posts->the_post(); ?>
                 
-                        <?php endwhile;  ?>
-                    
-                    </div> <!-- .row  -->
+                        <!-- Content in the loop -->
+                        <div class="col-10 col-md-8 col-lg-4 offset-1 offset-md-2 offset-lg-1 post-content">
+                            <a><?php the_category(); ?></a>
+                            <div class="thumbnail-container">
+                                <div class="thumbnail">
+                                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                                    <?php 
+                                    if ( has_post_thumbnail() ): 
+
+                                        //Creates array that points to the source of the posts thumbnail
+                                        $image_src = wp_get_attachment_image_src( get_post_thumbnail_id(),’thumbnail’ );
+                                        //Sets class to img src which now can be styled directly via class
+                                        echo '<img class="thumbnail-image" src="' . $image_src[0] . '">';
+                                            
+                                    endif; ?>
+                                    </a>
+                                </div> <!--./thumbnail-->
+                            </div> <!--./thumbnail-container-->
+                            <?php echo '<h1><a href="'.get_permalink( $id ).'">'.get_the_title( $id ).'</a></h1>'; ?>
+                            <p><?php the_excerpt(); ?></p>
+                            <a id=#readmore>Läs mer</a>                           
+                        </div> 
+            
+                    <?php endwhile;  ?>
+                
+                </div> <!-- .row  -->
                 
             <!-- Ends loop -->
             <?php endif; ?>
--- wp-content/themes/udde/home.php
+++ wp-content/themes/udde/home.php
@@ -18,11 +18,21 @@
                     <!-- Content in the loop -->
                     <div class="col post-content">
                         <a><?php the_category(); ?></a>
-                        <div class="thumbnail">
-                            <?php 
-                            // the_post_thumbnail(); 
-                            ?>
-                        </div>
+                        <div class="thumbnail-container">
+                            <div class="thumbnail">
+                                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                                <?php 
+                                if ( has_post_thumbnail() ): 
+                                    
+                                    //Creates array that points to the source of the posts thumbnail
+                                    $image_src = wp_get_attachment_image_src( get_post_thumbnail_id(),’thumbnail’ );
+                                    //Sets class to img src which now can be styled directly via class
+                                    echo '<img class="thumbnail-image" src="' . $image_src[0] . '">';
+                                        
+                                endif; ?>
+                                </a>
+                            </div> <!--./thumbnail-->
+                        </div> <!--./thumbnail-container-->
                         <?php echo '<h1><a href="'.get_permalink( $id ).'">'.get_the_title( $id ).'</a></h1>'; ?>
                         <p><?php the_excerpt(); ?></p>
                         <a id=#readmore>Läs mer</a>                           
@@ -40,18 +50,5 @@
         </div>
     </div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 <?php get_footer(); ?>

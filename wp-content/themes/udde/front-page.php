d9f9c50acfc5e7302e1378b092eaf0d9de277d64
Emma Uddesson <emma.uddesson@gmail.com>
2017-12-15 12:27:54 +0100

    New structure for front page-template

--- wp-content/themes/udde/front-page.php
+++ wp-content/themes/udde/front-page.php
@@ -1,14 +1,10 @@
-<!-- 
-Template name: Front Page
--->
-
 <?php get_header(); ?>
 
 <div class="front-page-header">
 
     <?php require 'parts/navbar.php'; ?>
     
-    <?php require 'parts/front-page-header-content.php'; ?>
+    <?php require 'parts/header-content-front-page.php'; ?>
 
 </div> <!-- .front-page-header -->
 
@@ -16,49 +12,7 @@ Template name: Front Page
     <div class="row">
         <div class="col-12">
 
-            <?php 
-            /** Creates a new query to display only four posts when this template page is used.
-            Also uses 'no_found_rows=true' to avoid the loop from counting the total matching rows in db,
-            which could slow down the site when db has more content.
-            On the topic I found this helpful: https://flavio.tordini.org/speed-up-wordpress-get_posts-and-query_posts-functions
-            **/
-            $display_four_posts = new WP_Query( 'no_found_rows=true&posts_per_page=4' ); 
-
-            if ( $display_four_posts->have_posts() ) : ?>
-                <div class="row">
-
-                    <!-- Loop -->
-                    <?php  while ( $display_four_posts->have_posts() ) : $display_four_posts->the_post(); ?>
-                
-                        <!-- Content in the loop -->
-                        <div class="col-10 col-md-8 col-lg-4 offset-1 offset-md-2 offset-lg-1 post-content">
-                            <a><?php the_category(); ?></a>
-                            <div class="thumbnail-container">
-                                <div class="thumbnail">
-                                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-                                    <?php 
-                                    if ( has_post_thumbnail() ): 
-
-                                        //Creates array that points to the source of the posts thumbnail
-                                        $image_src = wp_get_attachment_image_src( get_post_thumbnail_id(),’thumbnail’ );
-                                        //Sets class to img src which now can be styled directly via class
-                                        echo '<img class="thumbnail-image" src="' . $image_src[0] . '">';
-                                            
-                                    endif; ?>
-                                    </a>
-                                </div> <!--./thumbnail-->
-                            </div> <!--./thumbnail-container-->
-                            <?php echo '<h1><a href="'.get_permalink( $id ).'">'.get_the_title( $id ).'</a></h1>'; ?>
-                            <p><?php the_excerpt(); ?></p>
-                            <a id=#readmore>Läs mer</a>                           
-                        </div> 
-            
-                    <?php endwhile;  ?>
-                
-                </div> <!-- .row  -->
-                
-            <!-- Ends loop -->
-            <?php endif; ?>
+            <?php require 'parts/post-content-front-page.php' ?>
 
         </div> <!-- /.col-12 -->
     </div> <!-- /.row -->
---
+++ wp-content/themes/udde/parts/post-content-front-page.php
@@ -0,0 +1,44 @@
+<!-- Content of blog posts -->
+<?php 
+/** Creates a new query to display only four posts when this template page is used.
+Also uses 'no_found_rows=true' to avoid the loop from counting the total matching rows in db,
+which could slow down the site when db has more content.
+On the topic I found this helpful: https://flavio.tordini.org/speed-up-wordpress-get_posts-and-query_posts-functions
+**/
+$display_four_posts = new WP_Query( 'no_found_rows=true&posts_per_page=4' ); 
+
+if ( $display_four_posts->have_posts() ) : ?>
+    <div class="row justify-content-md-center">
+
+        <!-- Loop -->
+        <?php  while ( $display_four_posts->have_posts() ) : $display_four_posts->the_post(); ?>
+    
+            <!-- Content in the loop -->
+            <div class="col-10 col-md-8 col-lg-5 post-content">
+                <a><?php the_category(); ?></a>
+                <div class="thumbnail-container">
+                    <div class="thumbnail">
+                        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                        <?php 
+                        if ( has_post_thumbnail() ): 
+
+                            //Creates array that points to the source of the posts thumbnail
+                            $image_src = wp_get_attachment_image_src( get_post_thumbnail_id(),’thumbnail’ );
+                            //Sets class to img src which now can be styled directly via class
+                            echo '<img class="thumbnail-image" src="' . $image_src[0] . '">';
+                                
+                        endif; ?>
+                        </a>
+                    </div> <!--./thumbnail-->
+                </div> <!--./thumbnail-container-->
+                <?php echo '<h1><a href="'.get_permalink( $id ).'">'.get_the_title( $id ).'</a></h1>'; ?>
+                <p><a href="<?php the_permalink(); ?>"><?php the_excerpt(); ?></p></a>
+                <a href="<?php the_permalink(); ?>" id="readmore">Läs mer</a>                           
+            </div> 
+
+        <?php endwhile;  ?>
+    
+    </div> <!-- .row  -->
+    
+<!-- Ends loop -->
+<?php endif; ?>

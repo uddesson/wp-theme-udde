86a4792d14c0a912c973ec6fc756937d0284e729
Emma Uddesson <emma.uddesson@gmail.com>
2017-12-17 12:24:58 +0100

    Replacing sidebar with "filter by" categories on top of page

--- wp-content/themes/udde/home.php
+++ wp-content/themes/udde/home.php
@@ -6,6 +6,11 @@
 
 <?php echo 'DET HÄR ÄR PORTFOLIO/HOME'; ?>
 
+<div class="col-12 col-lg-10 offset-lg-1 filterby-container">
+    <h1>filter_by();</h1>
+    <?php wp_list_categories(); ?>
+</div>
+
 <div class="container-fluid feed-container">
     <div class="row">
             <?php require 'parts/post-content-archive.php' ?>

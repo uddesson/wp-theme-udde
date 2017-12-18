6865b1997abbac6744f91fc4865d0e021a0c01c9
Emma Uddesson <emma.uddesson@gmail.com>
2017-12-18 14:01:08 +0100

    Adjusted loop for single posts and styled images

--- wp-content/themes/udde/parts/single-post-content.php
+++ wp-content/themes/udde/parts/single-post-content.php
@@ -1,26 +1,36 @@
 <?php if ( have_posts() ) : ?> 
 
-<!-- Loops out all the posts -->
-<?php while ( have_posts() ) : the_post(); ?>
-    
+    <!-- Loops out the post -->
+    <?php while ( have_posts() ) : the_post(); ?>
+        
     <!-- Content in the loop -->
-    <div class="col post-content">
-        <a><?php the_category(); ?></a>
-        <div class="thumbnail">
-            <?php 
-            // the_post_thumbnail(); 
-            ?>
+    <div class="col-10 offset-1 single-post-content">
+        <div class=post-categories>
+                <a><?php the_category('<a>/</a>'); ?></a>
         </div>
+
+        <div class="single-post-thumbnail-container">
+            <div class="single-post-thumbnail">
+            
+                <?php 
+                if ( has_post_thumbnail() ): 
+
+                    /* Fetches the post thumbnail in original resolution 
+                    and assigns it to my class single-post-thumbnail-image */
+                    the_post_thumbnail('full', array('class' => 'single-post-thumbnail-image')); 
+
+                endif; ?>
+            
+            </div> <!--./single-post-thumbnail-->
+        </div> <!--./single-post-thumbnail-container-->   
+        
+        <!-- Fetches and formats title, excerpt and post content from database -->
         <h1><?php the_title(); ?></h1> 
         <p><?php the_excerpt(); ?></p>   
-        <p><?php the_content(); ?></p>                         
-    </div>
+        <p><?php the_content(); ?></p>                           
+    </div> <!-- ./single-post-content -->
+        
+    <?php endwhile;  ?>
 
-    
-<?php endwhile;  ?>
-
-
-</div>
-
-            <!-- Ends loop -->
-            <?php endif; ?>
+<!-- Ends loop -->
+<?php endif; ?>

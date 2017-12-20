e1faa23567ec97ee3648f7ea1744713e8c5fe489
Emma Uddesson <emma.uddesson@gmail.com>
2017-12-20 13:59:02 +0100

    Layout for about page

--- wp-content/themes/udde/about.php
+++ wp-content/themes/udde/about.php
@@ -1,25 +1,25 @@
 <!-- 
-Template name: About 
+Template name: Page with sidebar
 -->
 
 <?php get_header(); ?>
 
-<div class="container-fluid nav-container">
+<div class="container-fluid nav-container-invert">
 	<?php require 'parts/navbar.php'; ?>
 </div>
 
-	<div class="row">
-
-		<div class="col-sm-8 posts-main">
-
-		<h1> (Template: About) ah, about mig alltså </h1>
-		<p><?php the_author_meta( 'description' ); ?></p>
-		
-
-		</div> <!-- /.blog-main -->
-
+<!-- This row contains both content and sidebar -->
+<div class="row"> 
+	
+	<div class="col-7 offset-1 content">
+		<?php require 'parts/page-content.php'; ?>
+	</div> 
+	
+	<div class="col-3">
+		<!-- Displays the content of the sidebar -->
 		<?php get_sidebar(); ?>
+	</div>
 
-	</div> <!-- /.row -->
+</div> <!-- /.row -->
 
 <?php get_footer(); ?>

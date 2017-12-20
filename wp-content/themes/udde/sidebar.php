9a8a3c8f27323d1e77872ad41ef5cd8bc31936d1
Emma Uddesson <emma.uddesson@gmail.com>
2017-12-20 13:59:53 +0100

    Added content in sidebar + styling

--- wp-content/themes/udde/sidebar.php
+++ wp-content/themes/udde/sidebar.php
@@ -1,14 +1,9 @@
-<div class="sidebar-module">
-    <h2>Kategorier?</h2>
-    <ol class="list-unstyled">
-        <?php wp_get_archives( 'type=monthly' ); ?>
-    </ol>
-</div>
-
-<div class="sidebar-module">
-    <h2>Länkar woop</h2>
-    <ol class="list-unstyled">
-        <li><a href="https://github.com/uddesson">GitHub</a></li>
-        <li><a href="https://linkedin.com/emmauddesson">LinkedIn</a></li>
-    </ol>
+<div class="sidebar">
+    <div class="sidebar-module">
+        <h2>explore_work();</h2>
+        <div class="categories-sidebar">
+            <a><?php wp_list_categories(); ?></a>
+        </div>
+        
+    </div>
 </div>

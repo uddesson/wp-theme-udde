64197cc86c5ae040623bb08392fed42ba8091870
Emma Uddesson <emma.uddesson@gmail.com>
2017-12-21 15:28:20 +0100

    Rearranged loop for page-content, fixed mistake

--- wp-content/themes/udde/parts/page-content.php
+++ wp-content/themes/udde/parts/page-content.php
@@ -1,24 +1,26 @@
 <!-- To use the function the_content, which echos the page content,
 we must also use this while-loop --> 
 <?php while ( have_posts() ) : the_post(); ?>
-
-    <div class="content-thumbnail-container">
-        <div class="col-12 content-thumbnail">
             
-            <!-- Checks if page has thumbnail/featured image -->
-            <?php if ( has_post_thumbnail() ): 
+    <!-- Checks if page has thumbnail/featured image -->
+    <?php if ( has_post_thumbnail() ): ?>
 
+        <div class="content-thumbnail-container">
+            <div class="col-12 content-thumbnail">
+
+                <?php 
                 /* Fetches the (optional) page thumbnail in original resolution 
                 and assigns it to the class single-post-thumbnail-image */
                 the_post_thumbnail('full', array('class' => 'content-thumbnail-image')); 
+                ?>
 
-            endif; ?>
+            </div> <!--./page-thumbnail-->
+        </div> <!--./page-thumbnail-container -->   
 
-        <!-- Displays the content of the page -->
-        <?php the_content(); ?>
-        
-        </div> <!--./page-thumbnail-->
-    </div> <!--./page-thumbnail-container -->   
+    <?php endif; ?>
+
+<!-- Displays the content of the page -->
+<?php the_content(); ?>
 
 <!-- Ends loop -->
 <?php endwhile; ?>

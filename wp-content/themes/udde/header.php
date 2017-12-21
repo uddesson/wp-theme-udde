9820a0d46b04c8df70fd26b47804e826d3616b88
Emma Uddesson <emma.uddesson@gmail.com>
2017-12-21 16:19:31 +0100

    Changed the html language from sv to en

--- wp-content/themes/udde/header.php
+++ wp-content/themes/udde/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="sv">
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -20,7 +20,7 @@
 
     <!-- Used for anchor link -->
     <div id="jump-to-top"></div>
-    
+
     <?php wp_head();?>
   </head>
   <body>

4ba7b1159f7d8686ba28a2f9abd9653a8c4aba10
Emma Uddesson <emma.uddesson@gmail.com>
2017-12-21 15:58:22 +0100

    Changed content + styling for 404

--- wp-content/themes/udde/404.php
+++ wp-content/themes/udde/404.php
@@ -1,9 +1,14 @@
 <?php get_header(); ?>
 
-<div class="container-fluid nav-container">
-    <?php require 'parts/navbar.php'; ?>
+<div class="container-fluid nav-container-invert">
+	<?php require 'parts/navbar.php'; ?>
 </div>
 
-<h1> 404 </h1>
+<div class="got-lost-container">
+    <div class="lost-message-box">
+        <h1>404</h1>
+        <p>Woops, there's nothing here. </3</p>
+    </div>
+</div>
 
 <?php get_footer(); ?>

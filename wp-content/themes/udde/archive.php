dd9ba712a03ce29afd8819535229c90d25595927
Emma Uddesson <emma.uddesson@gmail.com>
2018-10-11 22:35:04 +0200

    Restructure content blocks

--- wp-content/themes/udde/archive.php
+++ wp-content/themes/udde/archive.php
@@ -8,14 +8,16 @@ Template name: Archive view
     <?php require 'parts/navbar.php'; ?>
 </div>
 
-<div class="col-10 offset-1 filterby-container">
-    <h1>filter_portfolio_by();</h1>
-    
-    <!-- Displays all categories that has posts in them -->
-    <?php wp_list_categories('title_li='); ?>
-</div>
 
 <div class="container-fluid feed-container">
+
+  <div class="col-10 offset-1 offset-lg-2 filterby-container">
+      <h1>filter_portfolio_by();</h1>
+
+      <!-- Displays all categories that has posts in them -->
+      <?php wp_list_categories('title_li='); ?>
+  </div>
+
     <div class="row">
         <?php require 'parts/post-content-archive.php' ?>
     </div>
--- wp-content/themes/udde/home.php
+++ wp-content/themes/udde/home.php
@@ -4,17 +4,18 @@
     <?php require 'parts/navbar.php'; ?>
 </div>
 
-<div class="col-10 offset-1 filterby-container">
-    <h1>filter_portfolio_by();</h1>
-
-    <!-- Displays all categories that has posts in them -->
-    <?php wp_list_categories('title_li='); ?>
-</div>
-
 <div class="container-fluid feed-container">
-    <div class="row">
-        <?php require 'parts/post-content-archive.php' ?>
-    </div>
+
+  <div class="col-10 offset-1 offset-lg-2 filterby-container">
+      <h1>filter_portfolio_by();</h1>
+
+      <!-- Displays all categories that has posts in them -->
+      <?php wp_list_categories('title_li='); ?>
+  </div>
+
+  <div class="row">
+      <?php require 'parts/post-content-archive.php' ?>
+  </div>
 </div>
 
 <div class="col-10 offset-1 pagination-links">
